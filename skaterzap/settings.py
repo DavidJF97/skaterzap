6286ade37a164d47f408f76e9ddc44edf602d2a5
David J-F <davidjf97@yahoo.ie>
2023-07-04 18:17:07 +0000

    Set up hidden folder

--- skaterzap/settings.py
+++ skaterzap/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 if os.path.exists("env.py"):
     import env
 
@@ -22,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+from django.conf import settings
+
+settings.SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
