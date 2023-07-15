01468c68c42440f371bf9f5edfd9145d4be77f5b
David J-F <davidjf97@yahoo.ie>
2023-07-15 11:14:44 +0000

    Added Toast

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -12,6 +15,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -32,13 +36,14 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
 
 
 def adjust_bag(request, item_id):
-    """ Adjust quantity of the specified product to specified amount """
+    """Adjust the quantity of the specified product to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
     size = None
@@ -62,8 +67,9 @@ def adjust_bag(request, item_id):
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
+
 def remove_from_bag(request, item_id):
-    """ Remove the specified product """
+    """Remove the item from the shopping bag"""
 
     try:
         size = None
@@ -72,12 +78,9 @@ def remove_from_bag(request, item_id):
         bag = request.session.get('bag', {})
 
         if size:
-            if quantity > 0:
-                bag[item_id]['items_by_size'][size] = quantity
-            else:
-                del bag[item_id]['items_by_size'][size] 
-                if not bag[item_id]['items_by_size']:
-                    bag.pop(item_id)
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
         else:
             bag.pop(item_id)
 
