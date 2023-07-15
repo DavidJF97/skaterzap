c41a1d897604e216dcfa01b63ada33064ea23d73
David J-F <davidjf97@yahoo.ie>
2023-07-15 23:53:00 +0000

    rewrite call for secret key in settings

--- skaterzap/settings.py
+++ skaterzap/settings.py
@@ -24,10 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from django.conf import settings
 
-settings.SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = settings.SECRET_KEY
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
